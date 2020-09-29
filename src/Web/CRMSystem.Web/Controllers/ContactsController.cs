47cc0b1edcb3abedd2e9a03305978022309a1ec0
tonkatawe <vlahov.anton@gmail.com>
2020-09-29 16:32:46 +0300

    Scaffolded login identity and correct loginView

---
+++ src/Web/CRMSystem.Web/Controllers/ContactsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CRMSystem.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ContactsController:Controller
+    {
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create()
+        {
+            //TODO: Make Create Input Model and so on
+
+            throw new NotImplementedException();
+        }
+    }
+}
